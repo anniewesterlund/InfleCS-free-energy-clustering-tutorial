c54fb8be4584b4472f0b7efdef558af1d3d317f7
anniewesterlund <34099005+anniewesterlund@users.noreply.github.com>
2019-06-13 12:59:54 +0200

    Delete __init__.py

--- GMM_FE/__init__.py
+++
@@ -1,4 +0,0 @@
-from .GMM_free_energy import FreeEnergy
-from .FE_landscape_clustering import LandscapeClustering
-from .stack_landscapes import LandscapeStacker
-from .free_energy_pathways import FreeEnergyPathways
